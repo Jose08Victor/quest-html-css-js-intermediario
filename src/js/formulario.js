1505682938aa721df2c6f85f5d987aee05285481
José Victor <jose08.victor@gmail.com>
2023-11-05 20:53:15 -0300

    Refatorei o código JS

--- src/js/formulario.js
+++ src/js/formulario.js
@@ -6,11 +6,9 @@ formulario.addEventListener("submit", (e) => {
 
     camposDoFormulario.forEach((input) => {
         if (input.children[0].value) {
-            input.classList.remove("invalid");
-            input.classList.add("valid");
+            input.className = "input valid";
         } else {
-            input.classList.remove("valid");
-            input.classList.add("invalid");
+            input.className = "input invalid";
         }
     });
 });
